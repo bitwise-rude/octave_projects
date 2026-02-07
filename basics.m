af14737a1fe2aaf9e2187d3e74469d00d658f02f
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2026-02-07 13:25:57 +0545

    some basics, how everything is a vector

---
+++ basics.m
@@ -0,0 +1,16 @@
+% no data types everythign is this
+a = 5;
+b = 3.14;
+
+% arrays are everything
+v = [1 2 3 4]; % row vectors
+u = [1; 2; 3; 4]; % column vectors
+
+A = [1 2; 3 4]; % 2x2 matrix
+
+% without semicolon it gets displayed
+
+%v(1)
+%A(1,2)
+
+
