d23ad971e1b5c273e05a68a4af68edd0c2ad72be
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2026-02-09 07:14:57 +0545

    new added

---
+++ day3.m
@@ -0,0 +1,49 @@
+%x = [1 2 3 4 5];
+%y = 1:5;
+
+        
+%z = linspace(0,10,6);
+
+
+
+%t = 0:0.001:3*pi;
+%y = sin(5*t);
+%y2 = cos(5*t);
+
+%plot(t,y,"r.","LineWidth",4);
+%hold on;
+%plot(t,y2,"b-");
+
+%legend("Sin wave", "Cos Wave");
+%grid on;
+
+%xlabel("t->");
+%ylabel("sin or cos");
+
+
+%x = 5;
+
+%if  x > 4
+    %disp("WOWO");
+%else
+    %disp("NOW");
+%end
+
+%for i = 1:5
+    %disp(i);
+%end
+
+%x = 1;
+%while x < 100
+    %disp(x);
+    %x =  x + 1;
+%end
+
+%function y = nice(x)
+    %y = 5;
+%end
+
+
+
+pause;
+
